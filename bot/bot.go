29b2a039d1f0bc44a778767118816234db4c25e3
Jakub Schier <jakub.schier@gmail.com>
2023-02-04 14:01:34 +0100

    prep for horde size func, ignore config

--- bot/bot.go
+++ bot/bot.go
@@ -24,7 +24,7 @@ func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
     switch {
         // list all commands here TODO: maybe some better structure
         case strings.Contains(message.Content, CommandPrefix + "help"):
-            discord.ChannelMessageSend(message.ChannelID, "Available commands: \n !help \n !status \n !logouts \n !logins \n !system")
+            discord.ChannelMessageSend(message.ChannelID, "Available commands: \n !help \n !status \n !logouts \n !logins \n !system \n !horde")
         // system returns basic system stats from "top"
         case strings.Contains(message.Content, CommandPrefix +"system"):
             out, err := exec.Command("bash", "-c", "top -b -n 1 | egrep 'top -|Tasks:|%Cpu|MiB'").Output()
@@ -34,19 +34,24 @@ func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
             discord.ChannelMessageSend(message.ChannelID, string(out[:]))
         // status returns the systemd status section relevant to the query
         case strings.Contains(message.Content, CommandPrefix +"status"):
-            out, err := exec.Command("bash", "-c", "sudo systemctl status docker | grep Active").Output()
+            out, err := exec.Command("bash", "-c", "sudo systemctl status zomboid.service | grep Active").Output()
             if err != nil {
                 log.Fatal("Encountered error while executing system command: ", err)
             }
             discord.ChannelMessageSend(message.ChannelID, string(out[:]))
         // see all logouts contained in the current log file TODO: limit to ~ last 10
         case strings.Contains(message.Content, CommandPrefix +"logouts"):
-            for _, line := range parser.ProcessLogFile("./sample_log.txt", "[disconnect]") {
+            for _, line := range parser.ProcessLogFile("/home/plesivsson/Zomboid/Logs/24-01-23_15-47-00_DebugLog-server.txt", "[disconnect]") {
                 discord.ChannelMessageSend(message.ChannelID, line)
             }
         // see all logins contained in the current log file TODO: limit to ~ last 10
+        // TODO: file location and name should be configurable (rather directory containing logs should
         case strings.Contains(message.Content, CommandPrefix +"logins"):
-            for _, line := range parser.ProcessLogFile("./sample_log.txt", "[fully-connected]") {
+            for _, line := range parser.ProcessLogFile("/home/plesivsson/Zomboid/Logs/24-01-23_15-47-00_DebugLog-server.txt", "[fully-connected]") {
+                discord.ChannelMessageSend(message.ChannelID, line)
+            }
+        case strings.Contains(message.Content, CommandPrefix +"horde"):
+            for _, line := range parser.ProcessLogFile("/home/plesivsson/Zomboid/Logs/24-01-23_15-47-00_DebugLog-server.txt", "wave") {
                 discord.ChannelMessageSend(message.ChannelID, line)
             }
     }
